25524220627f8ce4b25e27f28fc21c6fff88ee2b
Victor Martinez <correo.vmp@gmail.com>
2019-03-02 21:07:51 +0100

    courses service accede al backend

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,22 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Course } from './course.model';
 import { CoursesService } from './courses.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent implements OnInit {
+export class CoursesComponent implements OnInit, OnDestroy {
 
   courses: Course[];
+  private coursesSub: Subscription;
 
   constructor(private coursesService: CoursesService, private route: ActivatedRoute,
     private router: Router) { }
 
   ngOnInit() {
-    this.courses = this.coursesService.getCourses();
+    this.coursesService.getCourses();
+    this.coursesSub = this.coursesService.getCoursesUpdateListener()
+      .subscribe(transformedQoutasData => {
+        this.courses = transformedQoutasData.courses;
+      });
   }
 
+  ngOnDestroy() {
+    this.coursesSub.unsubscribe();
+  }
 }
